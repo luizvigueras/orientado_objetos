54d7158ac7b20a580b4e34640aaba799e7bb8901
Luis Pomposo V <Luis Pomposo V@LUISV-TI>
2016-06-13 19:44:20 -0500

    13062016

--- src/excepciones/archivo.java
+++ src/excepciones/archivo.java
@@ -4,7 +4,17 @@
  * and open the template in the editor.
  */
 package excepciones;
-
+import java.awt.HeadlessException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static java.util.stream.DoubleStream.builder;
+import static java.util.stream.LongStream.builder;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Luis Pomposo V
@@ -29,36 +39,99 @@ public class archivo extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setText("SELECCIONA UN ARCHIVO ");
 
         jButton1.setText("EXAMINAR");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("LEER ARCHIVO");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(72, 72, 72)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(171, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(32, 32, 32)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(45, 45, 45)
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(108, 108, 108)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(79, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(100, 100, 100)
+                .addGap(87, 87, 87)
                 .addComponent(jLabel1)
-                .addGap(65, 65, 65)
-                .addComponent(jButton1)
+                .addGap(78, 78, 78)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
                 .addContainerGap(98, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        JFileChooser selector=new JFileChooser();
+        selector.showOpenDialog(this);
+        
+        File f= selector.getSelectedFile();
+        String nombre= f.getName();
+                int indice= nombre.indexOf(".");
+                String extencion= nombre.substring(indice+1, nombre.length());
+                //validamos la extencion
+                
+        
+      try {
+          validarArchivo.validarExtencion(extencion);
+          FileInputStream fis=new FileInputStream(f);
+          int ch=0;
+           StringBuilder builder=new StringBuilder();
+            StringBuilder StringBuilder = new StringBuilder();
+            try {
+                while((ch=fis.read())!=-1){
+                    builder.append((char)ch);
+                }
+                long tama= builder.toString().getBytes().length;
+                String mensaje="SE LEYO BIEN Y SE ENCONTRO ESTO:"+builder.toString();
+                        JOptionPane.showMessageDialog(null, "extension\n"+extencion,"informacion",JOptionPane.INFORMATION_MESSAGE);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null,e.getMessage(), "extension\n"+extencion,"informacion",JOptionPane.ERROR_MESSAGE);
+            }
+          
+      } catch (FileNotFoundException ex) {
+            Logger.getLogger(archivo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        JOptionPane.showMessageDialog(null, "Elige un archivo", "Pronto habra algo", 
+                JOptionPane.INFORMATION_MESSAGE);
+        
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -97,6 +170,7 @@ public class archivo extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
